e4f74b7efc7844dcf81fdd94c88238e2a5cfcc3d
Marco Hess <marco.hess@through-ip.com>
2012-04-30 10:16:07 +0930

    Generate the home_text from a template so we can expand the host URL from the configuration settings. Update the install script to also expand this template.

--- install-gitweb-templates.sh
+++ install-gitweb-templates.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-rm -rf /etc/e-smith/templates/etc/gitweb.conf/*
-cp -r  root/etc/e-smith/templates/etc/gitweb.conf/* /etc/e-smith/templates/etc/gitweb.conf/
-chown  -R root:root /etc/e-smith/templates/etc/gitweb.conf
+rm -rf   /etc/e-smith/templates/etc/gitweb.conf
+mkdir -p /etc/e-smith/templates/etc/gitweb.conf
+cp -r    root/etc/e-smith/templates/etc/gitweb.conf/* /etc/e-smith/templates/etc/gitweb.conf/
+chown -R root:root /etc/e-smith/templates/etc/gitweb.conf
+
+rm -rf   /etc/e-smith/templates/etc/e-smith/web/common/gitweb_home_text.html
+mkdir -p /etc/e-smith/templates/etc/e-smith/web/common/gitweb_home_text.html
+cp -r    root/etc/e-smith/templates/etc/e-smith/web/common/gitweb_home_text.html/* /etc/e-smith/templates/etc/e-smith/web/common/gitweb_home_text.html
+chown -R root:root /etc/e-smith/templates/etc/e-smith/web/common/gitweb_home_text.html
+
+expand-template /etc/e-smith/web/common/gitweb_home_text.html
 expand-template /etc/gitweb.conf
+
 less -N /etc/gitweb.conf
