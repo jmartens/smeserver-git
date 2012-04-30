3e0f1263b3678de9bd5b553f91afd735bc2da6b8
Marco Hess <marco.hess@through-ip.com>
2012-04-30 11:28:43 +0930

    Update to also install custom gitweb resources for a better look.

--- install-http-templates.sh
+++ install-http-templates.sh
@@ -4,6 +4,14 @@ rm -rf /etc/e-smith/templates/etc/httpd/conf/httpd.conf/80SubDomainGit
 rm -rf /etc/e-smith/templates-custom/etc/httpd/conf/httpd.conf/80SubDomainGit
 cp -r  root/etc/e-smith/templates/etc/httpd/conf/httpd.conf/80SubDomainGit /etc/e-smith/templates/etc/httpd/conf/httpd.conf/
 chown  -R root:root /etc/e-smith/templates/etc/httpd/conf/httpd.conf/80SubDomainGit
+
+rm -rf /etc/e-smith/web/common/git*
+cp -r  root/etc/e-smith/web/common/git* /etc/e-smith/web/common
+chown  -R root:root /etc/e-smith/web/common/git*
+chmod  444 /etc/e-smith/web/common/git*
+
+expand-template /etc/gitweb.conf
+expand-template /etc/e-smith/web/common/gitweb_home_text.html
 expand-template /etc/httpd/conf/httpd.conf
 
 /usr/sbin/apachectl -t
