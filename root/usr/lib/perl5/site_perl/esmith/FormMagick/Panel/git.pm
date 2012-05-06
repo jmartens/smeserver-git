a2caf146e6e683cc7020460b12841e3f93e677a9
Marco Hess <marco.hess@through-ip.com>
2012-05-06 16:36:46 +0930

    * access settings are changed so we only select between local and internet. * all db attribute names are now all consistent lower case only for both the git db as the git entry in the config database * renamed the db attribute names for clarity. We now have things like 'allow_access_from', 'pull_groups', 'push_users', etc. * groups and users for pull and push are now listed in the repository table on the home screen with group names shown in bold.

--- root/usr/lib/perl5/site_perl/esmith/FormMagick/Panel/git.pm
+++ root/usr/lib/perl5/site_perl/esmith/FormMagick/Panel/git.pm
@@ -62,6 +62,7 @@ our @EXPORT = qw(
   git_repository_validate_description
 
   git_repository_print_name_field
+  git_repository_print_groups_and_users
   git_repository_print_privileges
   
   git_repository_group_list
@@ -176,10 +177,11 @@ sub git_repository_print_add_button {
 sub git_repository_print_table {
   my $self                     = shift;
   my $q                        = $self->{cgi};
-  my $name                     = $self->localise('NAME');
+  my $name                     = $self->localise('GIT_REPOSITORY_NAME');
   my $description              = $self->localise('DESCRIPTION');
-  my $access                   = $self->localise('ACCESS');
-  
+  my $access                   = $self->localise('GIT_ALLOW_FROM');
+  my $pull                     = $self->localise('GIT_PULL_ACCESS');
+  my $push                     = $self->localise('GIT_PUSH_ACCESS');
   my $modify                   = $self->localise('MODIFY');
   my $remove                   = $self->localise('REMOVE');
   my $action_h                 = $self->localise('ACTION');
@@ -196,32 +198,38 @@ sub git_repository_print_table {
 
   print qq(<tr><td colspan="2">);
   print $q->start_table({-CLASS => "sme-border"}),"\n";
-  print $q->Tr (
-                esmith::cgi::genSmallCell($q, $name,"header"),
-                esmith::cgi::genSmallCell($q, $description,"header"),
-                esmith::cgi::genSmallCell($q, $access,"header"),
-                esmith::cgi::genSmallCell($q, $action_h,"header", 3)), "\n";
+  print $q->Tr ( esmith::cgi::genSmallCell($q, $name,        "header"),
+                 esmith::cgi::genSmallCell($q, $description, "header"),
+                 esmith::cgi::genSmallCell($q, $access,      "header"),
+                 esmith::cgi::genSmallCell($q, $pull,        "header"),
+                 esmith::cgi::genSmallCell($q, $push,        "header"),
+                 esmith::cgi::genSmallCell($q, $action_h,    "header", 2) ), "\n";
   
   my $scriptname = basename($0);
 
   foreach my $repository (@repositories)
   {
-    my $repo_name                    = $repository->key();
-    my $repo_description             = $repository->prop('Description');
-    my $repo_access_type             = $repository->prop('AccessType');
+    my $repo_name              = $repository->key();
+    my $repo_description       = $repository->prop('description');
+    my $repo_allow_access_from = $self->git_repository_access_allowed_from($repository->prop('allow_access_from'));
+    
+    my $repo_pull              = $self->git_repository_print_groups_and_users( $repository->prop('pull_groups'), 
+                                                                               $repository->prop('pull_users') );
+    my $repo_push              = $self->git_repository_print_groups_and_users( $repository->prop('push_groups'),
+                                                                               $repository->prop('push_users') );
+    
+    my $params                 = $self->build_repository_cgi_params($repo_name, $repository->props());
+    my $href                   = "$scriptname?$params&action=modify&wherenext=";
+    my $actionModify           = '&nbsp;' . $q->a({href => "${href}GitCreateModify"},$modify) . '&nbsp;';
+    my $actionRemove           = '&nbsp;' . $q->a({href => "${href}GitRemove"}, $remove) . '&nbsp';
 
-    my $params = $self->build_repository_cgi_params($repo_name, $repository->props());
-    my $href = "$scriptname?$params&action=modify&wherenext=";
-    my $actionModify = '&nbsp;' . $q->a({href => "${href}GitCreateModify"},$modify) . '&nbsp;';
-    my $actionRemove = '&nbsp;' . $q->a({href => "${href}GitRemove"}, $remove) . '&nbsp';
-
-    print $q->Tr (
-        esmith::cgi::genSmallCell($q, $repo_name . ".git", "normal"),
-        esmith::cgi::genSmallCell($q, $repo_description, "normal"),
-        esmith::cgi::genSmallCell($q, $repo_access_type, "normal"),
-
-        esmith::cgi::genSmallCell($q, $actionModify,"normal"),
-        esmith::cgi::genSmallCell($q, $actionRemove,"normal"));
+    print $q->Tr (  esmith::cgi::genSmallCell($q, $repo_name . ".git",     "normal"),
+                    esmith::cgi::genSmallCell($q, $repo_description,       "normal"),
+                    esmith::cgi::genSmallCell($q, $repo_allow_access_from, "normal"),
+                    esmith::cgi::genSmallCell($q, $repo_pull,              "normal"),
+                    esmith::cgi::genSmallCell($q, $repo_push,              "normal"),
+                    esmith::cgi::genSmallCell($q, $actionModify,           "normal"),
+                    esmith::cgi::genSmallCell($q, $actionRemove,           "normal") ), "\n";
   }
 
   print $q->end_table,"\n";
@@ -313,12 +321,12 @@ sub git_repository_print_name_field {
     my $q = $self->{cgi};
     if ($repository)
     {
-      $q->param(-name=>'description',             -value=>$repository->prop('Description'));
-      $q->param(-name=>'access_type',             -value=>$repository->prop('AccessType'));
-      $q->param(-name=>'groupsPull',              -value=>join(FS, split(FS, $repository->prop('GroupsPull'))));
-      $q->param(-name=>'usersPull',               -value=>join(FS, split(FS, $repository->prop('UsersPull'))));
-      $q->param(-name=>'groupsPush',              -value=>join(FS, split(FS, $repository->prop('GroupsPush'))));
-      $q->param(-name=>'usersPush',               -value=>join(FS, split(FS, $repository->prop('UsersPush'))));
+      $q->param(-name=>'description',       -value=>$repository->prop('description'));
+      $q->param(-name=>'allow_access_from', -value=>$repository->prop('allow_access_from'));
+      $q->param(-name=>'pull_groups',       -value=>join(FS, split(FS, $repository->prop('pull_groups'))));
+      $q->param(-name=>'pull_users',        -value=>join(FS, split(FS, $repository->prop('pull_users'))));
+      $q->param(-name=>'push_groups',       -value=>join(FS, split(FS, $repository->prop('push_groups'))));
+      $q->param(-name=>'push_users',        -value=>join(FS, split(FS, $repository->prop('push_users'))));
     }
   } else {
     print qq(
@@ -407,49 +415,49 @@ sub git_repository_handle_create {
     return $self->error($msg);
   }
 
-  $msg = $self->validate_radio($self->cgi->param('access_type'));
+  $msg = $self->validate_radio($self->cgi->param('allow_access_from'));
   unless ($msg eq "OK")
   {
     return $self->error($msg);
   }
 
   my $groups_allowed_to_pull = "";
-  my @groupsPull = $self->cgi->param('groupsPull');
-  foreach my $grp_pull (@groupsPull) {
+  my @pull_groups = $self->cgi->param('pull_groups');
+  foreach my $pull_group (@pull_groups) {
     if ($groups_allowed_to_pull) {
-      $groups_allowed_to_pull .= "," . $grp_pull;
+      $groups_allowed_to_pull .= "," . $pull_group;
     } else {
-      $groups_allowed_to_pull = $grp_pull;
+      $groups_allowed_to_pull = $pull_group;
     }
   }
 
   my $users_allowed_to_pull = "";
-  my @usersPull = $self->cgi->param('usersPull');
-  foreach my $usr_pull (@usersPull) {
+  my @pull_users = $self->cgi->param('pull_users');
+  foreach my $pull_user (@pull_users) {
     if ($users_allowed_to_pull) {
-      $users_allowed_to_pull .= "," . $usr_pull;
+      $users_allowed_to_pull .= "," . $pull_user;
     } else {
-      $users_allowed_to_pull = $usr_pull;
+      $users_allowed_to_pull = $pull_user;
     }
   }
 
   my $groups_allowed_to_push = "";
-  my @groupsPush = $self->cgi->param('groupsPush');
-  foreach my $grp_push (@groupsPush) {
+  my @push_groups = $self->cgi->param('push_groups');
+  foreach my $push_group (@push_groups) {
     if ($groups_allowed_to_push) {
-      $groups_allowed_to_push .= "," . $grp_push;
+      $groups_allowed_to_push .= "," . $push_group;
     } else {
-      $groups_allowed_to_push = $grp_push;
+      $groups_allowed_to_push = $push_group;
     }
   }
 
   my $users_allowed_to_push = "";
-  my @usersPush = $self->cgi->param('usersPush');
-  foreach my $usr_push (@usersPush) {
+  my @push_users = $self->cgi->param('push_users');
+  foreach my $push_user (@push_users) {
     if ($users_allowed_to_push) {
-      $users_allowed_to_push .= "," . $usr_push;
+      $users_allowed_to_push .= "," . $push_user;
     } else {
-      $users_allowed_to_push = $usr_push;
+      $users_allowed_to_push = $push_user;
     }
   }
   
@@ -459,13 +467,13 @@ sub git_repository_handle_create {
   
   if (my $repository = $git_db->new_record($repositoryName, 
        {
-          Description => $self->cgi->param('description'),
-          GroupsPull  => "$groups_allowed_to_pull",
-          UsersPull   => "$users_allowed_to_pull",
-          GroupsPush  => "$groups_allowed_to_push",
-          UsersPush   => "$users_allowed_to_push",
-          AccessType  => $self->cgi->param('access_type'),
-          type        => 'repository',
+          Description       => $self->cgi->param('description'),
+          pull_groups       => "$groups_allowed_to_pull",
+          pull_users        => "$users_allowed_to_pull",
+          push_groups       => "$groups_allowed_to_push",
+          push_users        => "$users_allowed_to_push",
+          allow_access_from => $self->cgi->param('allow_access_from'),
+          type              => 'repository',
         } ) )
   {
     # Untaint $name before use in system()
@@ -496,61 +504,61 @@ sub git_respository_handle_modify {
     return $self->error($msg);
   }
 
-  $msg = $self->validate_radio($self->cgi->param('access_type'));
+  $msg = $self->validate_radio($self->cgi->param('allow_access_from'));
   unless ($msg eq "OK")
   {
     return $self->error($msg);
   }
 
   my $groups_allowed_to_pull = "";
-  my @groupsPull = $self->cgi->param('groupsPull');
-  foreach my $grp_pull (@groupsPull) {
+  my @pull_groups = $self->cgi->param('pull_groups');
+  foreach my $pull_group (@pull_groups) {
     if ($groups_allowed_to_pull) {
-      $groups_allowed_to_pull .= "," . $grp_pull;
+      $groups_allowed_to_pull .= "," . $pull_group;
     } else {
-      $groups_allowed_to_pull = $grp_pull;
+      $groups_allowed_to_pull = $pull_group;
     }
   }
 
   my $users_allowed_to_pull = "";
-  my @usersPull = $self->cgi->param('usersPull');
-  foreach my $usr_pull (@usersPull) {
+  my @pull_users = $self->cgi->param('pull_users');
+  foreach my $pull_user (@pull_users) {
     if ($users_allowed_to_pull) {
-      $users_allowed_to_pull .= "," . $usr_pull;
+      $users_allowed_to_pull .= "," . $pull_user;
     } else {
-      $users_allowed_to_pull = $usr_pull;
+      $users_allowed_to_pull = $pull_user;
     }
   }
 
   my $groups_allowed_to_push = "";
-  my @groupsPush = $self->cgi->param('groupsPush');
-  foreach my $grp_push (@groupsPush) {
+  my @push_groups = $self->cgi->param('push_groups');
+  foreach my $push_group (@push_groups) {
     if ($groups_allowed_to_push) {
-      $groups_allowed_to_push .= "," . $grp_push;
+      $groups_allowed_to_push .= "," . $push_group;
     } else {
-      $groups_allowed_to_push = $grp_push;
+      $groups_allowed_to_push = $push_group;
     }
   }
 
   my $users_allowed_to_push = "";
-  my @usersPush = $self->cgi->param('usersPush');
-  foreach my $usr_push (@usersPush) {
+  my @push_users = $self->cgi->param('push_users');
+  foreach my $push_user (@push_users) {
     if ($users_allowed_to_push) {
-      $users_allowed_to_push .= "," . $usr_push;
+      $users_allowed_to_push .= "," . $push_user;
     } else {
-      $users_allowed_to_push = $usr_push;
+      $users_allowed_to_push = $push_user;
     }
   }
 
   if (my $repository = $git_db->get($repositoryName)) {
     if ($repository->prop('type') eq 'repository') {
-      $repository->merge_props( Description => $self->cgi->param('description'),
-                                GroupsPull  => "$groups_allowed_to_pull",
-                                UsersPull   => "$users_allowed_to_pull",
-                                GroupsPush  => "$groups_allowed_to_push",
-                                UsersPush   => "$users_allowed_to_push",
-                                AccessType  => $self->cgi->param('access_type'),
-                                type        => 'repository',
+      $repository->merge_props( Description       => $self->cgi->param('description'),
+                                pull_groups       => "$groups_allowed_to_pull",
+                                pull_users        => "$users_allowed_to_pull",
+                                push_groups       => "$groups_allowed_to_push",
+                                push_users        => "$users_allowed_to_push",
+                                allow_access_from => $self->cgi->param('allow_access_from'),
+                                type              => 'repository',
                                 );
 
       # Untaint $name before use in system()
@@ -623,7 +631,7 @@ sub getExtraParams
     my $repository = $git_db->get($repositoryName);
     if ($repository)
     {
-      $repositoryDescription = $repository->prop('Description');
+      $repositoryDescription = $repository->prop('description');
     }
   }
   return (name => $repositoryName, description => $repositoryDescription);
@@ -636,7 +644,7 @@ sub getExtraParams
 # Returns OK on success or a localised error message otherwise.
 
 sub git_repository_validate_name {
-  my ($self, $repositoryName) = @_;
+  my( $self, $repositoryName ) = @_;
   unless ($repositoryName =~ /^([a-z][\_\-a-z0-9]*)$/)
   {
     return $self->localise('GIT_ERROR_NAME_HAS_INVALID_CHARS',
@@ -646,20 +654,64 @@ sub git_repository_validate_name {
 }
 
 #----------------------------------------------------------------------
+# git_repository_access_allowed_from()
+#
+# Checks that the name supplied does not contain any unacceptable chars.
+# Returns OK on success or a localised error message otherwise.
+
+sub git_repository_access_allowed_from {
+  my ($self, $accessAllowedFrom) = @_;
+
+  if( $accessAllowedFrom eq 'internet' ) {
+    return $self->localise( 'GIT_ACCESS_ALLOWED_FROM_INTERNET' );
+  } elsif( $accessAllowedFrom eq 'local' ) {
+    return $self->localise( 'GIT_ACCESS_ALLOWED_FROM_LOCAL' );
+  } else {
+    return $self->localise( 'GIT_ACCESS_ALLOWED_FROM_CONFIG_ERROR' );
+  }
+}
+
+#----------------------------------------------------------------------
+# git_repository_print_groups_and_users()
+#
+# Formats a print string for use in the repository table listing the groups and users
+# that are allowed access. Groups are printed in bold. When there are no groups or users
+# it returns Anonymous.
+
+sub git_repository_print_groups_and_users {
+  my ($self, $groups, $users) = @_;
+
+  unless( $groups || $users ) {
+    return "<i>" . $self->localise( 'GIT_ANONYMOUS' ) . "</i>";
+  }
+   
+  my $print_groups = "";
+  if( $groups ) {
+    $print_groups =  "<b>" . join("<br/>", split(FS, $groups)) . "</b><br/>";
+  }
+  my $print_users = "";
+  if( $users ) {
+    $print_users =  join("<br/>", split(FS, $users));
+  }
+  
+  return $print_groups . $print_users;    
+}
+
+#----------------------------------------------------------------------
 # git_repository_validate_name_does_not_exist()
 # Check the proposed repository name for clashes with existing respositories.
 
 sub git_repository_validate_name_does_not_exist
 {
-  my ($self, $repositoryName) = @_;
-  my $repository = $git_db->get($repositoryName);
+  my( $self, $repositoryName ) = @_;
+  my $repository = $git_db->get( $repositoryName );
 
   if (defined $repository)
   {
     my $type = $repository->prop('type');
-    if ($type eq "repository")
+    if( $type eq "repository" )
     {
-      return $self->localise('GIT_ERROR_ALREADY_EXISTS', {repositoryName => $repositoryName});
+      return $self->localise( 'GIT_ERROR_ALREADY_EXISTS', { repositoryName => $repositoryName } );
     }
   }
   # Repository does not exist yet.
@@ -674,19 +726,19 @@ sub git_repository_validate_name_does_not_exist
 # maximum length of $self->{defaultMaxLength} if nothing is set in the 
 # config db.
 sub git_repository_validate_name_length {
-  my ($self, $data) = @_;
+  my( $self, $data ) = @_;
   $config_db->reload();
   my $max;
-  if (my $max_record = $config_db->get('maxRepositoryNameLength')) {
+  if( my $max_record = $config_db->get( 'maxRepositoryNameLength' ) ) {
     $max = $max_record->value();
   }
 
-  if (length($data) <= $max) {
+  if( length($data) <= $max ) {
     return "OK";
   } else {
-    return $self->localise("GIT_ERRROR_NAME_TOO_LONG",
-                            {repositoryName => $data,
-                             maxRepositoryNameLength => $max });
+    return $self->localise( "GIT_ERRROR_NAME_TOO_LONG",
+                            { repositoryName => $data,
+                              maxRepositoryNameLength => $max } );
   }
 }
 
@@ -696,10 +748,10 @@ sub git_repository_validate_name_length {
 # Checks that the description supplied does not contain any unacceptable chars.
 # Returns OK on success or a localised error message otherwise.
 sub git_repository_validate_description {
-  my ($self, $repositoryDescription) = @_;
-  unless ($repositoryDescription =~ /^([\w\s\_\.\-]*)$/) {
+  my( $self, $repositoryDescription ) = @_;
+  unless( $repositoryDescription =~ /^([\w\s\_\.\-]*)$/ ) {
     return $self->localise('GIT_ERROR_DESCRIPTION_HAS_INVALID_CHARS',
-                           {repositoryDescription => $repositoryDescription});
+                           { repositoryDescription => $repositoryDescription } );
   }
   return "OK";
 }
@@ -709,9 +761,10 @@ sub git_repository_validate_description {
 # Checks wether a value is checked for a radio button
 
 sub validate_radio {
-  my ($self, $acctName) = @_;
-  unless($acctName ne '') {
-    return $self->localise('GIT_ERROR_RADIO_VALUE_NOT_CHECKED', acctName => $acctName);
+  my( $self, $acctName ) = @_;
+  unless( $acctName ne '' ) {
+    return $self->localise( 'GIT_ERROR_RADIO_VALUE_NOT_CHECKED', 
+                            { acctName => $acctName } );
   }
   return "OK";
 }
